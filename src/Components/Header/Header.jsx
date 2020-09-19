881ada64689c16606c191072e0ea44623a5f33f6
Jordania Pereyra <Jordania.Pereyra@gmail.com>
2020-09-19 18:00:40 -0400

    header css update

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,14 +3,15 @@ import "./Header.css";
 
 function Header() {
   return (
-    <div>
+    <section className="header">
       <img
         src="https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1597228681809x513873629664535900%2Fimage%2520%25286%2529.png?w=256&h=67&auto=compress&fit=crop&dpr=0.75"
         alt=""
+        className="header__logo"
       />
 
-      <ul className="icon-list">
-        <li className="icon-item">
+      <ul className="header__icon-list">
+        <li className="header__icon-item">
           <a
             href="https://www.linkedin.com/company/aspirent-group/"
             className="header-icon-link"
@@ -21,7 +22,7 @@ function Header() {
           </a>
         </li>
 
-        <li className="icon-item">
+        <li className="header__icon-item">
           <a
             href="https://twitter.com/aspirentg"
             className="header-icon-link"
@@ -33,8 +34,8 @@ function Header() {
         </li>
       </ul>
 
-      <button>Login</button>
-    </div>
+      <button className="header__button">Login</button>
+    </section>
   );
 }
 
