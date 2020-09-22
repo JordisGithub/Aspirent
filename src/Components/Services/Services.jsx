8a6f0bac156329fc35cd900b3c7479ffc49f971b
Jordania Pereyra <Jordania.Pereyra@gmail.com>
2020-09-22 17:35:49 -0400

    Added services component css

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -13,37 +13,43 @@ function Services() {
               srcset=""
             />
           </div>
-          <div>Connect</div>
-          <div>
-            A network of entrepreneurs, industry experts and corporates working
-            together to solve real business problems.
+          <div className="services-grid__title">Connect</div>
+          <div className="services-grid__description">
+            A network of entrepreneurs, industry experts and <br /> <br />
+            corporates working together to solve real business problems.
           </div>
         </div>
 
         <div className="services-grid__item">
-          <img
-            src="https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1597229710681x396598722615631100%2Fcollaborate-icon.png?w=384&h=343&auto=compress&fit=crop&dpr=0.75"
-            alt=""
-            srcset=""
-          />
-          <div>Collaborate</div>
-          <div>
-            Trade resources and work together with entrepreneurs, corporates and
-            startups. Create collaboration partnerships on our unique platform.
+          <div className="services-grid__image">
+            <img
+              src="https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1597229710681x396598722615631100%2Fcollaborate-icon.png?w=384&h=343&auto=compress&fit=crop&dpr=0.75"
+              alt=""
+              srcset=""
+            />
+            <div className="services-grid__title">Collaborate</div>
+            <div className="services-grid__description">
+              Trade resources and work together with entrepreneurs, corporates
+              and startups.
+              <br /> <br />
+              Create collaboration partnerships on our unique platform.
+            </div>
           </div>
         </div>
 
         <div className="services-grid__item">
-          <img
-            src="https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1597229728716x252933379400354530%2Fcocreate-icon.png?w=384&h=343&auto=compress&fit=crop&dpr=0.75"
-            alt=""
-            srcset=""
-          />
-          <div>Co-create</div>
-          <div>
-            Innovate with the help of entrepreneurs & corporates to solve real
-            problems. create investment in startups where you see the highest
-            return.
+          <div className="services-grid__image">
+            <img
+              src="https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1597229728716x252933379400354530%2Fcocreate-icon.png?w=384&h=343&auto=compress&fit=crop&dpr=0.75"
+              alt=""
+              srcset=""
+            />
+            <div className="services-grid__title">Co-create</div>
+            <div className="services-grid__description">
+              Innovate with the help of entrepreneurs & corporates to solve real
+              problems. <br /> <br /> create investment in startups where you
+              see the highest return.
+            </div>
           </div>
         </div>
       </section>
