67f1af1af5018aa73b3b78da5d62856fff7621ef
Jordania Pereyra <Jordania.Pereyra@gmail.com>
2020-09-23 10:21:19 -0400

    Added About Us component css

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import "./AboutUs.css";
 
 function AboutUs() {
-  return <div className="aboutus"></div>;
+  return (
+    <div className="aboutus">
+      <div className="aboutus__title">Teamwork is our passion</div>
+      <div className="aboutus__paragaph">
+        We're obsessively passionate about bringing individuals from different
+        walks of life to solve problems together. It's in our DNA. <br /> <br />
+        Our mission is to help people to come together to achieve a common goal
+        using our app to connect, collaborate and co-create, all in one
+        platform.
+      </div>
+    </div>
+  );
 }
 
 export default AboutUs;
